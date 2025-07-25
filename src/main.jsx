3624559f8675d71622cb4dd64d3a01cd8971b9e5
Sally M <smei201900@gmail.com>
2025-07-24 21:00:37 -0400

    added more comments explaining what the default code is doing as I am learning

--- src/main.jsx
+++ src/main.jsx
@@ -9,8 +9,14 @@ createRoot(document.getElementById('root')).render(
   </StrictMode>,
 )
 
+//.jsx allows developers to write in HTML-LIKE code within JS File 
+
 /*StrictMode allows React to run extra checks and provide warnings aka for debugging.
-  createRoot allows us to essentially display things  
+  createRoot allows us to essentially display things; it creates our React Project
+*/
+
+/*we find the element in our HTML file that contains the ID of "root" 
+    Look at index.html for that ID
 */
 
 /*
