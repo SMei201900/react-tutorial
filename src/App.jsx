4917a6f50643f35ae5f32b8958109a9253aa6279
Sally M <smei201900@gmail.com>
2025-07-29 21:15:19 -0400

    learning useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Footer from "./components/Footer";
 import BookList from "./components/BookList";
+import SearchBar from "./components/SearchBar";
 
 const list = [
   {
@@ -19,12 +20,14 @@ const list = [
     author: 'mr.krabs'
   },
 ];
+
 function App() {
-  const [bookList, setBookList] = useState([...list]);
+  const [bookList, setBookList] = useState([...list]); 
   const [searchTitle, setSearchTitle] = useState("hi");
   const filteredBooks = bookList.filter((book) => book.title.toLowerCase().includes(searchTitle.toLowerCase()));
   return (
     <section>
+      <SearchBar searchTitle={searchTitle} setSearchTitle={setSearchTitle} />
       <BookList list={filteredBooks} />
       <Footer />
     </section>
@@ -34,12 +37,25 @@ function App() {
 export default App
 
 
+
 /*export default App
   export default is you putting ONE main tool into a tool box with NO special labeling.
   When someone asks for "the tool," you know its just that one thing inside the box.*/
 
 /*useState is a function provided by react.
   We are calling it with    import { useState } from 'react' 
-  It is used to keep track and manage functional components. 
+  It is used to keep track and manage functional components 
+    AKA it tells the program what to remember and potentially display 
   Here it is used to change the counter when I click the button. 
 */
+
+/*const [bookList, setBookList] = useState([...list]);  
+    "const" here meaning we created a variable 
+    The variable is "bookList"
+    "setBookList" is a function which we also created with "const"
+      It is created so that if I want to CHANGE the value of the variable, 
+      I can use this function to do so. 
+*/
+
+//The ... in   useState([...list]);   means we made a copy of the "list" 
+
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const SearchBar = ({ searchTitle, setSearchTitle }) => {
+    // This function updates searchTitle when user types into text input
+    const handleChange = (e) => {
+        setSearchTitle(e.target.value);
+    }
+
+  return (
+    <div>
+        {/* Added a label and text input as UI elements a user can enter text into */}
+        <label htmlFor="searchTitle">
+            <p>Search for Book by title</p>
+            <input type="text" name="searchTitle" id="searchTitle" placeholder="Ex: Tom Sawyer" value={searchTitle} onChange={handleChange} />
+        </label>
+    </div>
+    
+  )
+}
+
+export default SearchBar
+
+// {/*this is how you comment inside JS*/} 
