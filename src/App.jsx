7e7253818ae0b28ca89d8474c81678260dbbd6c6
Sally M <smei201900@gmail.com>
2025-08-04 20:06:27 -0400

    used axios and toast to get bookstore API and added comments to explain code

--- src/App.jsx
+++ src/App.jsx
@@ -3,38 +3,42 @@ import Footer from "./components/Footer";
 import BookList from "./components/BookList";
 import SearchBar from "./components/SearchBar";
 import axios from "axios"
-import { ToastContainer } from "react-toastify";
-
-const list = [
-  {
-    id: 1,
-    title: 'hi',
-    author: 'bye'
-  },
-  {
-    id: 2,
-    title: 'joe',
-    author: 'mama'
-  },
-    {
-    id: 3,
-    title: 'autobiography of eugene krabs',
-    author: 'mr.krabs'
-  },
-];
+import { ToastContainer, toast } from "react-toastify";
 
 function App() {
-  const [bookList, setBookList] = useState([...list]); 
+  const [bookList, setBookList] = useState([]); 
   //setting searchtitle to item of searchTitle from localStorage if it exists. If not we have a fallback value of "hi"
   const [searchTitle, setSearchTitle] = useState(localStorage.getItem("searchItem") || "hi");
-  const filteredBooks = bookList.filter((book) => book.title.toLowerCase().includes(searchTitle.toLowerCase())
+  const filteredBooks = bookList.filter(
+    (book) => book.title.toLowerCase().includes(searchTitle.toLowerCase())
     || book.author.toLowerCase().includes(searchTitle.toLowerCase()));
   
-    useEffect(() => {
-      //this function updates the searchTitle value in localStorage everytime the searchTitle variable is updated in the application
-      localStorage.setItem("searchTitle", searchTitle); 
+  useEffect(() => {
+    //this function updates the searchTitle value in localStorage everytime the searchTitle variable is updated in the application
+    localStorage.setItem("searchTitle", searchTitle); 
     }, {searchTitle}); 
     //by adding searchTitle in the dependency array everytime the seachTitle variable gets changed, we call the useEffect function 
+  
+  useEffect(() => {
+    async function getBooks() { //async function to get books from bookstore API 
+      try { 
+        const response = await axios.get("https://bookstore-api-six.vercel.app/api/books")
+        /*instead of fetch and json.response, we can do this*/
+        /*using axios to send HTTP GET request to the endppoint and storing it in a variable called "response"*/
+        setBookList(response.data); 
+        /*using setBookList function to update bookList to response.data from API*/
+        toast.success("Success fetching books");
+        //using toast.success to let the UI know this request was successful 
+      } catch (error) {
+        console.log(error.message);
+        //using console.log to console error message if it occurs 
+        toast.error("Error fetching books"); 
+        //using toast.error to let the UI know if the request was a failure 
+      }
+    }
+    getBooks(); //we need to call getBooks() after we have defined it in our function 
+  }, [] );  
+
 
   return (
     <section>
@@ -69,5 +73,7 @@ export default App
       I can use this function to do so. 
 */
 
-//The ... in   useState([...list]);   means we made a copy of the "list" 
+//The ... in  useState([...list]);   means we made a copy of the "list" 
 
+/* <ToastContainer /> under return() is required to use react-toastify. 
+It shows a pop-up with a delete button that has a green (or red) loading bar that will eventually disappear*/
